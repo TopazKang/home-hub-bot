46fc05bfd22cfb7c057d19ced989b7e9ff92004b
TopazKang <lasksla@gmail.com>
2026-08-09 13:13:42 +0900

    init: 초기 프로젝트 구성

---
+++ src/main/java/com/topazkang/homehubbot/HomeHubBotApplication.java
@@ -0,0 +1,13 @@
+package com.topazkang.homehubbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeHubBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeHubBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/topazkang/homehubbot/HomeHubBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topazkang.homehubbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeHubBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
